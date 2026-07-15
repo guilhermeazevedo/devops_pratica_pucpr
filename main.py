3fb2595b2a8b7ab2b3d1534ac4720d245b3636ae
Guilherme Augusto de Azevedo Torres <guilherme.azevedo.torres@hotmail.com>
2026-07-15 19:39:31 +0000

    Criado listar_tarefas

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from fastapi import FastAPI
+from datetime import datetime
 
+LISTA_TAREFAS = []
 APP = FastAPI()
+
+def nova_tarefa(id: int, titulo: str, descricao: str):
+    return {
+        "id": id,
+        "titulo": titulo,
+        "descricao": descricao,
+        "concluido": False,
+        "criado_em": datetime.now()
+    }
 
 @APP.get("/")
 def index():
     return "Olá, DevOps!"
 
-    
+
+@APP.get("/tarefas")
+def listar_tarefas():
+    global LISTA_TAREFAS
+
+    if len(LISTA_TAREFAS) == 0:
+        return LISTA_TAREFAS
+
+    tarefas = []
+
+    for tarefa in LISTA_TAREFAS:
+        info = {"id": tarefa['id'], "titulo": tarefa['titulo']}
+        tarefas.append(info)
+
+    return tarefas
